4954cd61e820ff499d15af12a585dd1c252456c9
Sietse Snel <s.t.snel@uu.nl>
2020-07-28 12:41:13 +0200

    various adjustments for reproducing #4624

--- issue-4624/create-and-recreate-testfiles.sh
+++ issue-4624/create-and-recreate-testfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-numfiles=10
+numfiles=1
 filesize_kb=10
 
 dd if=/dev/urandom bs=${filesize_kb}k count=1 of=testfile
@@ -10,20 +10,38 @@ for n in $(seq $numfiles)
     iput -f testfile "$testfile"
 done
 
-while true
-  do num=$(ils -L | grep -cE "consumerResource.+test\.[0-9]+\.txt") 
-     if [ "$num" -ge "$numfiles" ]
-     then echo "All files have been replicated."
-	  break
-     else echo "Currently $num out of $numfiles have been replicated. Waiting for"
-	  echo  "asynchronous replication to complete ..."
-	  sleep 2
-     fi
+for n in $(seq $numfiles)
+ do testfile="test.$n.txt"
+    echo "Replicating test file $testfile to consumer ..."
+    irepl -R consumerResource "/testZone/home/rods/$testfile"
 done
 
 for n in $(seq $numfiles)
  do testfile="test.$n.txt"
- echo "Deleting and re-creating test file $testfile ..."
- irm "$testfile"
+ echo "Deleting test file $testfile ..."
+ irm "test.$n.txt"
+done
+
+for n in $(seq $numfiles)
+ do testfile="test.$n.txt"
+    echo "Replicating trash copy of test file $testfile to consumer ..."
+    irepl -R consumerResource "/testZone/trash/home/rods/$testfile"
+done
+
+for n in $(seq $numfiles)
+ do testfile="test.$n.txt"
+ echo "Creating hard links in iCAT database for file $testfile ..."
+ sudo -u postgres psql -d ICAT -c "update r_data_main set coll_id = 10011 where data_repl_num = 1 and data_name = '$testfile';"
+done
+
+for n in $(seq $numfiles)
+ do testfile="test.$n.txt"
+ echo "Re-creating test file $testfile ..."
  iput -f testfile "test.$n.txt"
+done
+
+for n in $(seq $numfiles)
+ do testfile="test.$n.txt"
+ echo "Deleting test file $testfile ..."
+ irm "$testfile"
 done
