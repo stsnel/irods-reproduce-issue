35d95d196a369adbbba6b6456ec5aa16418da2ef
Sietse Snel <s.t.snel@uu.nl>
2020-08-14 13:53:22 +0200

    Various small improvements test file creation
    - Add parameter for test file prefix, so that Vagrant VMs can be easily
      reused for multiple tests.
    - Don't run irepl command for replicating trash copy of deleted test
      file, since it gets replicated automatically by iRODS.

--- issue-4624/create-and-recreate-testfiles.sh
+++ issue-4624/create-and-recreate-testfiles.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
 numfiles=1
 filesize_kb=10
-
-dd if=/dev/urandom bs=${filesize_kb}k count=1 of=testfile
+testfile_prefix="test"
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
-    echo "Creating test file $testfile ..."
-    iput -f testfile "$testfile"
+ do testfile="${testfile_prefix}.$n.txt"
+    echo "Creating and uploading file $testfile ..."
+    dd if=/dev/urandom bs=${filesize_kb}k count=1 of=${testfile_prefix}.$n.txt
+    iput -f "$testfile"
 done
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
+do  testfile="${testfile_prefix}.$n.txt"
     echo "Replicating test file $testfile to consumer ..."
     irepl -R consumerResource "/testZone/home/rods/$testfile"
 done
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
+ do testfile="${testfile_prefix}.$n.txt"
  echo "Deleting test file $testfile ..."
- irm "test.$n.txt"
+ irm "$testfile"
 done
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
-    echo "Replicating trash copy of test file $testfile to consumer ..."
-    irepl -R consumerResource "/testZone/trash/home/rods/$testfile"
-done
-
-for n in $(seq $numfiles)
- do testfile="test.$n.txt"
+ do testfile="${testfile_prefix}.$n.txt"
  echo "Creating hard links in iCAT database for file $testfile ..."
  sudo -u postgres psql -d ICAT -c "update r_data_main set coll_id = 10011 where data_repl_num = 1 and data_name = '$testfile';"
 done
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
+ do testfile="${testfile_prefix}.$n.txt"
  echo "Re-creating test file $testfile ..."
  iput -f testfile "test.$n.txt"
 done
 
 for n in $(seq $numfiles)
- do testfile="test.$n.txt"
+ do testfile="${testfile_prefix}.$n.txt"
  echo "Deleting test file $testfile ..."
  irm "$testfile"
 done
